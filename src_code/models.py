7adf8f9fb669d487897408acf22a267aa2e5bbc7
vungnv <vungnc@avtvn.com>
2024-05-01 14:34:43 +0700

    update src model

--- src_code/models.py
+++ src_code/models.py
@@ -1,6 +1,8 @@
 import uuid
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User as Auth_User
+from user import models as user_models
 
 # Create your models here.
 class LanguageCode(models.Model):
@@ -19,7 +21,11 @@ class SrcCode(models.Model):
     src_code_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=100)
     content = models.TextField()
+    image = models.ImageField(upload_to='srccode/', null=True, blank=True)
     languages = models.ManyToManyField(LanguageCode, related_name='src_code_language', blank=True)
+    created_on = models.DateTimeField(default=timezone.now)
+    created_by = models.ForeignKey(Auth_User, on_delete=models.CASCADE, related_name='src_code_created_by', null=True,
+                                   blank=True)
     created_on = models.DateTimeField(default=timezone.now)
     modified_on = models.DateTimeField(default=timezone.now)
 
@@ -28,10 +34,14 @@ class SrcCode(models.Model):
 
     def to_dict(self):
         language_names = [language.name for language in self.languages.all()]
+        author_user_profile = user_models.Profile.objects.filter(base_user=SrcCode.created_by).first()
+        author_user_id = author_user_profile.user_id_profile if author_user_profile else ''
         return {
             "src_code_id": self.src_code_id,
             "name": self.name,
             "content": self.content,
             "languages": language_names,
+            'created_by': author_user_id,
+            'created_by_image': author_user_profile.image.name if author_user_profile else None,
         }
     
