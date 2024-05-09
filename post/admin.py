c0a180391b1afeab609e12ac13e447f170521717
vungnv <vungnc@avtvn.com>
2024-05-09 17:00:34 +0700

    update srccode

--- post/admin.py
+++ post/admin.py
@@ -4,7 +4,7 @@ from .models import Post,CommentPost
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     search_fields = ['title']
-    list_display = ['title', 'created_by','post_id','spotlight']
+    list_display = ['title', 'created_by','post_id','spotlight', 'status']
     list_display_links = ['title']
     list_filter = []
 @admin.register(CommentPost)
--- src_code/models.py
+++ src_code/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User as Auth_User
 from user import models as user_models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class LanguageCode(models.Model):
@@ -20,7 +21,7 @@ class LanguageCode(models.Model):
 class SrcCode(models.Model):
     src_code_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=100)
-    content = models.TextField()
+    content = RichTextField(blank=True, null=True)
     image = models.ImageField(upload_to='srccode/', null=True, blank=True)
     languages = models.ManyToManyField(LanguageCode, related_name='src_code_language', blank=True)
     created_on = models.DateTimeField(default=timezone.now)
@@ -43,5 +44,7 @@ class SrcCode(models.Model):
             "language_ids":[language.id for language in self.languages.all()],
             'created_by': author_user_id,
             'created_by_image': author_user_profile.image.name if author_user_profile else None,
-        }
+            'created_by_name': author_user_profile.first_name + author_user_profile.last_name,
+            'created_by_chool': author_user_profile.school
+        }   
     
