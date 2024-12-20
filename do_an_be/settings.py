c662c4624f532cee3bf57814c9295f63534fd597
vungnv <vungnv@avtvn.com>
2024-12-20 10:51:03 +0700

    update term in admin

--- do_an_be/settings.py
+++ do_an_be/settings.py
@@ -30,6 +30,10 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://*127.0.0.1', 'http://127.0.0.1:*',
                         'http://*', 'https://*']
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173', 'http://localhost:5174'
+]
 
 
 # Application definition
